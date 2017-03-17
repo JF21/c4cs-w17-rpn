fd6d884248ec9dcbc9c1266e34cc8fa403256ae5
Justin <Justin@example.com>
2017-03-17 11:52:41 -0400

    updated python source

--- rpn.py
+++ rpn.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python3
+
 import operator
 
-
-operators = {
+OPERATORS = {
 	'+': operator.add,
 	'-': operator.sub,
+	'*': operator.mul,
+	'/': operator.truediv,
 }
+
 
 def calculate(arg):
 	stack = list()
-
 	for operand in arg.split():
 		try:
 			operand = float(operand)
 			stack.append(operand)
-		except ValueError:
+		except:
 			arg2 = stack.pop()
-			arg1 = stack.pop()	
-
-			stack.push(operators[operator](arg1, arg2))
-
+			arg1 = stack.pop()
+			operator_fn = OPERATORS[operand]
+			result = operator_fn(arg1, arg2)
+			
+			stack.append(result)
 	return stack.pop()
 
 def main():
 	while True:
-		result = calculate(input("rpn calc> "))
-		print("Result: ", result)
+		result = calculate(input('rpn calc> '))
+		print("Result:", result)
 
 if __name__ == '__main__':
 	main()
